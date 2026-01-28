c3e924e333220f9eb094f8f1c3c52aac62037524
rafkrawczyk <54412881+rafkrawczyk@users.noreply.github.com>
2026-01-28 14:30:15 +0100

    Add files via upload

---
+++ integerCheck.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 27 22:38:55 2026
+
+@author: rafal
+"""
+
+x = int(input('Enter an integer: '))
+if x % 2 == 0:
+    print('')
+    print('Even')
+else:
+    print('')
+    print('Odd')
+print('Done with conditional')
