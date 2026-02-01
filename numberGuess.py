aba0e2a331bf7e09e477031da686eaf014ea694c
rafkrawczyk <54412881+rafkrawczyk@users.noreply.github.com>
2026-02-01 13:58:24 +0100

    Add files via upload

---
+++ numberGuess.py
@@ -0,0 +1,21 @@
+high = 100
+low = 0
+
+print("Please think of a number between 0 and 100!")
+
+while True:
+    guess = (high + low) // 2
+    
+    print("Is your secret number " + str(guess) + "?")
+    
+    user_input = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. ")
+    
+    if user_input == 'c':
+        print("Game over. Your secret number was: " + str(guess))
+        break
+    elif user_input == 'l':
+        low = guess
+    elif user_input == 'h':
+        high = guess        
+    else:
+        print("Sorry, I did not understand your input.")
